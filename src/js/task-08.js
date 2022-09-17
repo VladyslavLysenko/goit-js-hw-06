e0080a23e4bc087bd234a46b2124b3460747fe47
VladyslavLysenko <Dodog12362@gmail.com>
2022-09-17 21:56:11 +0300

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,43 @@
+const form = document.querySelector(".login-form");
+
+form.setAttribute("action", "submit")
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+
+  event.preventDefault();
+
+  const email = event.currentTarget.elements.email.value;
+
+  const password = event.currentTarget.elements.password.value;
+
+
+
+
+  if (email == "" || password == "") {
+
+    alert("Please fill in all the fields!");
+
+  } else {
+
+    const data = {
+
+      email: email,
+
+      password: password,
+
+    };
+
+    console.log(data);
+
+    event.currentTarget.reset();
+
+  }
+
+}
+
+
+
+
 
